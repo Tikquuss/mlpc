28366d74e1e3c67885465026cbfd39d2e3946d5a
zeufack <zeufackp@gmail.com>
2020-12-12 14:23:20 +0100

    add the countdown js

---
+++ js/countdown.js
@@ -0,0 +1,26 @@
+const countDownDate = new Date("Mar 31, 2021 00:00:00").getTime();
+
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("hours").innerHTML = "EXPIRED";
+        document.getElementById("minutes").innerHTML = "EXPIRED";
+        document.getElementById("seconds").innerHTML = "EXPIRED";
+    }
+}, 1000);
